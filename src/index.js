c8959a2fbb5e84cc0618f7ac7d93ab8b702fd512
Michail <ganshinor@gmail.com>
2020-07-14 00:04:18 +0300

    ещё один обработчик

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Post from './Post';
 import json from './assets/json';
+import xml from './assets/data.xml';
 import logo from './assets/logo.png';
 import './styles/style.css';
 
@@ -8,3 +9,4 @@ const post = new Post ('Webpack Post Title', logo);
 console.log('Post to string', post.toString());
 
 console.log('JSON', json);
+console.log('XML', xml);
