5b01e0502adbbce31b3417184f2c7e82730eab09
Michail <ganshinor@gmail.com>
2020-07-15 15:34:33 +0300

    подключил jquery

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import Post from './Post';
-import json from './assets/json';
-import xml from './assets/data.xml';
-import logo from './assets/logo.png';
+import * as $ from 'jquery';
+import Post from '@models/Post';
+// import json from './assets/json';
+// import xml from './assets/data.xml';
+import logo from '@/assets/logo.png';
 import './styles/style.css';
 
 const post = new Post ('Webpack Post Title', logo);
+$('pre').html(post.toString())
 
-console.log('Post to string', post.toString());
+//console.log('Post to string', post.toString());
 
-console.log('JSON', json);
-console.log('XML', xml);
+// console.log('JSON', json);
+// console.log('XML', xml);
